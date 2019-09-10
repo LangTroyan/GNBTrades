0aacb52d5b1ad9e3caa1a40ae79eddc919dfbb17
Dylan Trujillo <langtroyan@gmail.com>
2019-09-11 00:50:04 +0200

    Initial Commit

---
+++ GNB Trades/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GNB Trades
+//
+//  Created by Lang Troyan on 11/09/2019.
+//  Copyright © 2019 Lang Troyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
